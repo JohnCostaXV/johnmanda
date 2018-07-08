157912bb8f5aa2943b624aba6691cca90dccfc5e
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-08 20:09:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -472,13 +472,13 @@ async def on_message(message):
 
     if message.content.startswith('/tempmute'):
         if '407677666750365706' or '417426253658849281' or '407678188773179417' or '407678481670078475' or '407706417282416641' in [role.id for role in message.author.roles]:
+            user = message.mentions[0]
             args = message.content.split(" ")
             tempo = (" ".join(args[3:]))
             channel = client.get_channel('448449971629588481')
             role = discord.utils.get(user.server.roles, name="Silenciado")
             await client.add_roles(user, role)
             print("Adicionado o cargo '" + role.name + "' para " + user.name)
-            user = message.mentions[0]
             timesquad = int(tempo)
             reallytime = "{}".format(datetime.timedelta(seconds=timesquad))
             embed = discord.Embed(
