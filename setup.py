796d7b516e9fd9c0cabf8a79fdc57618e2125ba1
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-21 17:19:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -320,6 +320,7 @@ async def on_message(message):
             pass
 
     if message.content.lower().startswith('/end'):
+        await client.delete_message(message)
         user = message.mentions[0]
         args = message.content.split(" ")
         await client.change_nickname(user, " ".join(args[1]))
