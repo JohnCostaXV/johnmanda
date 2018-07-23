a3a8156721f7861d832cb214c0f5c6194cbadc60
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-23 10:57:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1102,7 +1102,7 @@ async def on_message(message):
             #Cabeca
             cabeca = "https://crafatar.com/renders/head/" + uuid +"?default=HF_Steve&overlay.png"
 
-            head = discord.Embed(title='', color=COR, description='Clique [aqui](' + cabeca + ') para acessar a head do {}.'.format(nome))
+            head = discord.Embed(title='', color=COR, description='Head do {}.'.format(nome))
             head.set_image(url=cabeca)
             head.set_author(name=message.server.name, icon_url=message.server.icon_url)
             await client.send_message(message.channel, embed=head)
