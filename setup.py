cada63abc67f29853d20f11b48bc6696d5582855
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-23 12:10:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1164,6 +1164,17 @@ async def on_message(message):
                     time.sleep(timesquad)
                     cargo = discord.utils.get(user.server.roles, name='Silenciado')
                     await client.remove_roles(user, cargo)
+                else:
+                    embed2 = discord.Embed(
+                        title='Sem permissão!',
+                        color=COR,
+                        description='Você não tem permissão para executar esse comando.'
+                    )
+                    embed2.set_footer(text=message.author.name, icon_url=message.avatar_url)
+                    msg = await client.send_message(message.channel, embed=embed2)
+                    await client.delete_message(message)
+                    time.sleep(15)
+                    await client.delete_message(msg)
         except IndexError:
             msg = await client.send_message(message.channel, 'O usuário não é existente ou saiu!')
             time.sleep(10)
