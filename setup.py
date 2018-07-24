0f3fbd58906018cde3714def637619645c5618cb
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-24 12:14:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -325,10 +325,10 @@ async def on_message(message):
                     )
                     embed.set_author(name='Anúncio', icon_url='https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif')
                     embed.set_footer(
-                        text="Enviado por: {}  •  {}".format(message.author.name, datetime.utcnow()),
+                        text="Enviado por: {}  •  End".format(message.author.name),
                         icon_url=message.author.avatar_url
                     )
-                    #await client.send_message(message.channel, "@everyone")
+                    await client.send_message(message.channel, "@everyone")
                     await client.send_message(message.channel, embed=embed)
         except IndexError:
             await client.delete_message(message)
