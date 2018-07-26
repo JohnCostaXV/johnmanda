55658325de85cdb9b5f23534b283881114e05600
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-25 21:50:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -324,7 +324,7 @@ async def on_message(message):
                         description=" ".join(args[1:])
                     )
                     embed.set_author(name='Anúncio', icon_url='https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif')
-                    embed.timestamp(datetime.datetime.utcnow())
+                    embed.timestamp = datetime.datetime.utcnow()
                     embed.set_footer(
                         text="Enviado por: {}".format(message.author.name),
                         icon_url=message.author.avatar_url
