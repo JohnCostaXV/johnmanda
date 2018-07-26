37d87a2d4a8363b730aa0320f21aeaa682aa0267
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-25 21:20:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -256,6 +256,7 @@ async def on_message(message):
                 color=COR,
                 description='Use `/changenick [username] [nick]`'
             )
+            embedd.set_timestamp(message)
             embedd.set_footer(text=message.author.name, icon_url=message.author.avatar_url)
             await client.send_message(message.channel, embed=embedd)
         except:
@@ -323,12 +324,13 @@ async def on_message(message):
                         color=COR,
                         description=" ".join(args[1:])
                     )
+                    embed.set_timestamp(message)
                     embed.set_author(name='Anúncio', icon_url='https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif')
                     embed.set_footer(
-                        text="Enviado por: {}  •  End".format(message.author.name),
+                        text="Enviado por: {}".format(message.author.name),
                         icon_url=message.author.avatar_url
                     )
-                    await client.send_message(message.channel, "@everyone")
+                    #await client.send_message(message.channel, "@everyone")
                     await client.send_message(message.channel, embed=embed)
         except IndexError:
             await client.delete_message(message)
@@ -370,9 +372,10 @@ async def on_message(message):
                         description=" ".join(args[1:])
                     )
                     embed.set_footer(
-                        text="Enviado por: {}  •  End".format(message.author.name),
+                        text="Enviado por: {}".format(message.author.name),
                         icon_url='https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif'
                     )
+                    embed.set_timestamp(message)
                     await client.send_message(message.channel, "@everyone")
                     await client.send_message(message.channel, embed=embed)
         except IndexError:
@@ -420,6 +423,7 @@ async def on_message(message):
                     embed.set_thumbnail(
                         url='https://i.imgur.com/1iJeEea.jpg'
                     )
+                    embed.set_timestamp(message)
                     embed.set_footer(text='End', icon_url='https://i.imgur.com/1iJeEea.jpg')
                     await client.send_message(channel, embed=embed)
         except IndexError:
