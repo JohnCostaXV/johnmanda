909de7647b7cc6099bf8233406f4ed49253f1c08
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-28 09:32:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1301,10 +1301,7 @@ async def on_message(message):
             #Corpo
             corpo = "https://crafatar.com/renders/body/" + uuid +"?default=HF_Steve&overlay.png"
 
-            embed = discord.Embed()
-            embed.set_image(url=corpo)
-            time.sleep(5)
-            await client.send_message(message.channel, embed=embed)
+            await client.send_message(file=discord.File(corpo, filename='{}.png'.format(corpo)))
         except IndexError:
             embed = discord.Embed(
                 title='Comando incorreto!',
