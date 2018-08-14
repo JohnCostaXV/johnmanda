ed1b2a9dc557823a30b9ff22c9ec8f64aea1b206
UnixCF <41532859+unixcf@users.noreply.github.com>
2018-08-13 21:08:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1132,10 +1132,9 @@ async def on_message(message):
         embed.set_footer(text="End", icon_url="https://i.imgur.com/1iJeEea.jpg")
         await client.send_message(message.channel, embed=embed)
 
-    if message.content.lower().startswith('/info '):
+    if message.content.lower().startswith('/info'):
+        print message;
         try:
-            if (message.mentions[0]==null):
-              return "a"
             tmp1 = datetime.datetime.now()
 
             utcnow = datetime.time(hour=tmp1.hour, minute=tmp1.minute, second=tmp1.second)
@@ -1182,7 +1181,7 @@ async def on_message(message):
                 icon_url="https://i.imgur.com/1iJeEea.jpg"
             )
             await client.send_message(message.channel, embed=userembed)
-        except IndexError:
+        except IndexError
             await client.delete_message(message)
             msg = await client.send_message(message.channel, "{}, mencione um usuário existente, por exemplo, `/info @JohnnCosta`.".format(message.author.mention))
             time.sleep(10)
