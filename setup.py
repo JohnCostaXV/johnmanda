cfe6140a3a1961527a5e265b1f12948d0ae4c2a2
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-08-13 21:35:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1134,9 +1134,9 @@ async def on_message(message):
 
     if message.content.lower().startswith('/info'):
 	if (message.content.lower() == "/info"):
-	    msg = await client.send_message(message.channel, "{}, fuck, `fuck @JohnnCosta`.".format(message.author.mention))
-	    await asyncio.sleep(10)
-	    await client.delete_message(msg)
+		msg = await client.send_message(message.channel, "{}, fuck, `fuck @JohnnCosta`.".format(message.author.mention))
+		await asyncio.sleep(10)
+		await client.delete_message(msg)
         try:
             tmp1 = datetime.datetime.now()
 
