97d9b4e768dcb1357400f8ee5922b331b785f832
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-08-22 10:26:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -137,7 +137,6 @@ async def on_message(message):
         for r in message.author.roles:
             if r.id in cargos:
                 await client.delete_message(message)
-                canal = client.get_channel("448326186095869953")
                 await client.send_message(canal, "@everyone")
                 embed = discord.Embed(
                     color=COR,
@@ -146,7 +145,7 @@ async def on_message(message):
                 embed.set_footer(text="Enviado por: {}".format(message.author.name), icon_url="https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif")
                 embed.set_image(url="https://media.giphy.com/media/9Vxthk9QQ18MTNiwVk/giphy.gif")
                 embed.timestamp = datetime.datetime.utcnow()
-                await client.send_message(canal, embed=embed)
+                await client.send_message(message.channel, embed=embed)
     
     if message.content.lower().startswith("/esorteio"):
         cargos = [
