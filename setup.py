e8554da7366a9bff7013f48b7023ca5e05df45d9
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-08-24 11:31:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -138,6 +138,25 @@ def MCAPI(site):
 
 @client.event
 async def on_message(message):
+    if message.content.startswith("/voteagr"):
+        cargos = [
+            # IDs dos cargos:
+            "407677666750365706", #Diretor
+        ]
+        for r in message.author.roles:
+            if r.id in cargos:
+                await client.send_message(message.channel, "@everyone")
+                await client.delete_message(message)
+                embed = discord.Embed(
+                    color=COR,
+                    description="Faltam alguns minutos para a finalização da votação decisiva para a entrada ou não do VIP BETA em nossa loja durante o fim de semana. Você ainda votou? Não perca tempo, vote agora [clicando aqui](https://twitter.com/ServidorEnd/status/1032641301882306560)!"
+                )
+                embed.set_author(name="#END", icon_url="https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif")
+                embed.set_footer(text="Enviado por: {}".format(message.author.name), icon_url=message.author.avatar_url)
+                embed.timestamp = datetime.datetime.utcnow()
+                embed.set_image(url="https://i.imgur.com/qEcoExp.jpg")
+                await client.send_message(message.channel, embed=embed)
+
     if message.content.lower().startswith("/eanunciar"):
         cargos = [
             # IDs dos cargos:
