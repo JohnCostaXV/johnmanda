73b38c74334cf20c1a2d68159c0f82462b9325a5
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-06 13:35:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ async def on_reaction_add(reaction, user):
      await client.remove_reaction(msg, "✅", user)
      await client.delete_message(msg)
 
+
 @client.event
 async def randommessage():
     canal = client.get_channel('407669684616560650')
@@ -309,6 +310,26 @@ async def on_message(message):
         finally:
             pass
 
+    if message.content.lower().startswith("/faq"):
+        canal = client.get_channel("431992571686813706")
+        await client.delete_message(message)
+        embed = discord.Embed(
+            title="📖 FAQ - Servidores End",
+            color=COR,
+            description="**Como funciona**: Basta clicar na reação correspondente do item que você deseja!\n"
+                        "**Observação**: Caso você tenha dm privada você não receberá, então sinta-se livre em perguntar no {}".format(canal)
+        )
+        faq = discord.Embed(
+            title="Tópicos:",
+            color=COR,
+            description="💎 - Informações sobre **compras**\n"
+                        "📋 - Informações sobre **aplicações**"
+        )
+        await client.send_message(message.channel, embed=embed)
+        tst = await client.send_message(message.channel, embed=faq)
+        await client.add_reaction(tst, "💎")
+        await client.add_reaction(tst, "📋")
+    
     if message.content.lower().startswith('/helpstaff'):
         try:
             cargos = [
