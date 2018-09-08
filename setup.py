cd6af0b7dc835bb247f9c2e0126d765bf4d777f8
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-08 13:16:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1199,12 +1199,6 @@ async def on_message(message):
             finally:
                 pass
             
-        if message.content.lower().startswith('/endteste'):
-            cargos = len(message.server.roles)
-            emojis = len(message.server.emojis)
-            canais= len(message.server.channels)
-            membros = len(message.server.members)
-            await client.send_message(message.channel, "{}\n{}\n{}\n{}".format(cargos, emojis, canais, membros))
 
 
         if message.content.lower().startswith('/end!'):
@@ -1221,10 +1215,15 @@ async def on_message(message):
             else:
                 jogadores_online = Server['players']['now'];
 
+            cargos = len(message.server.roles)
+            emojis = len(message.server.emojis)
+            canais= len(message.server.channels)
+            membros = len(message.server.members)
+
             embed = discord.Embed(
                 title="Informações:",
                 color=COR,
-                descripition="**Servidor**:\n**IP**: jogar.end-mc.com\nJogadores online: {}/1500\n\n**Discord**:\n**ID**: {}\n**Criado em**: {}\n\n**CEOs**: Raaamos, Rosiello_, JohnnCosta, SeveBR e Mystherion.\n\n**Cargos**: {}\n**Emojis**: {}\n**Canais: {}\n\n**Usuários**: {}".format(str(jogadores_online), message.server.id, message.server.created_at.strftime("%d de %beiro de %Y, ás %H:%M"), len(message.server.roles), len(message.server.emojis), len(message.server.channels), len(message.server.members)))
+                descripition="**Servidor**:\n**IP**: jogar.end-mc.com\nJogadores online: {}/1500\n\n**Discord**:\n**ID**: {}\n**Criado em**: {}\n\n**CEOs**: Raaamos, Rosiello_, JohnnCosta, SeveBR e Mystherion.\n\n**Cargos**: {}\n**Emojis**: {}\n**Canais: {}\n\n**Usuários**: {}".format(str(jogadores_online), message.server.id, message.server.created_at.strftime("%d de %beiro de %Y, ás %H:%M"), cargos, emojis, canais, membros))
             embed.set_author(name="Servidores End", icon_url=message.server.icon_url)
             embed.set_thumbnail(url='https://i.imgur.com/1iJeEea.jpg')
             embed.timestamp = datetime.datetime.utcnow()
