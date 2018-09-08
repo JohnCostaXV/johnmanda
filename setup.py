a4e166cae3aef500bbe25df1e4c1ba4345a71f5c
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-08 13:33:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1242,11 +1242,12 @@ async def on_message(message):
             membros = len(message.server.members)
             players = str(jogadores_online)
             criadoem = str(message.server.created_at.strftime("%d de %beiro de %Y, ás %H:%M"))
+            icon = message.server.icon_url
 
             embed = discord.Embed(
                 title="Informações:",
                 color=COR,
-                description="**Servidor**:\n**IP**: jogar.end-mc.com\nJogadores online: {}/1500\n\n**Discord**:\n**ID**: {}\n**Criado em**: {}\n\n**CEOs**: Raaamos, Rosiello_, JohnnCosta, SeveBR e Mystherion.\n\n**Cargos**: {}\n**Emojis**: {}\n**Canais: {}\n\n**Usuários**: {}".format(players, message.server.id, criadoem, cargos, emojis, canais, membros))
+                description="**Servidor**:\n**IP**: jogar.end-mc.com\nJogadores online: {}/1500\n\n**Discord**:\n**ID**: {}\n**Criado em**: {}\n\n**CEOs**: Raaamos, Rosiello_, JohnnCosta, SeveBR e Mystherion.\n\n**Cargos**: {}\n**Emojis**: {}\n**Canais: {}\n\n**Usuários**: {}\n**Foto**: [Download] (" + icon + ")".format(players, message.server.id, criadoem, cargos, emojis, canais, membros))
             embed.set_author(name="Servidores End", icon_url=message.server.icon_url)
             embed.set_thumbnail(url='https://i.imgur.com/1iJeEea.jpg')
             embed.timestamp = datetime.datetime.utcnow()
