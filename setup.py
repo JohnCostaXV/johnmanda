7c6d2ea20ec0fdf1c1b7f6298d4b9165af17513b
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-10 23:24:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1306,8 +1306,10 @@ async def on_message(message):
                 description="**Servidor:**\n**IP**: jogar.end-mc.com\n**Jogadores online**: {}/1500\n\n**Discord:**\n**Foto**: [Download](".format(players) + icon + ")\n**ID**: {}\n**Criado em**: {}\n\n**CEOs**: Raaamos, Rosiello_, JohnnCosta, SeveBR e Mystherion.\n\n**Cargos**: {}\n**Emojis**: {}\n**Canais**: {}\n\n**Usuários**: {}".format(message.server.id, criadoem, cargos, emojis, canais, membros))
             embed.set_author(name="Servidores End", icon_url=message.server.icon_url)
             embed.timestamp = datetime.datetime.utcnow()
+            embed.set_image(url="https://status.minecraftservers.org/classic/512768.png")
             embed.set_footer(text='Comando por: {}'.format(message.author.name), icon_url="https://i.imgur.com/1iJeEea.jpg")
             await client.send_message(message.channel, embed=embed)
+        
 
         if message.content.lower().startswith('/info'):
             try:
