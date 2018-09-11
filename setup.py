af864e8fa8920ab2308e21e7a418dcd88c339485
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-11 10:38:06 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -81,7 +81,6 @@ async def on_member_join(member):
     embed.set_footer(text='Entrada')
     embed.timestamp = datetime.datetime.utcnow()
     await client.send_message(canal, embed=embed)
-    await client.send_message(canal, "https://twitter.com/ServidorEnd/status/1036461162047713280")
 
     global msg_id
     msg_id = react.id
