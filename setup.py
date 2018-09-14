c2af7665b870268b20d0cb490e163340386744c0
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-14 09:56:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -191,9 +191,8 @@ async def on_message(message):
         if message.content.lower().startswith("/emojizar"):
             args = message.content.split(" ")
             resposta = " ".join(args[1:])
-            #.replace("1", "⃣").replace("2", "⃣").replace("3", "3⃣").replace("4", "4⃣").replace("5", "5⃣").replace("6", "6⃣").replace("7", "7⃣").replace("8", "8⃣").replace("9", "9⃣").replace("0", "0⃣").replace("#", "#⃣")
 
-            await client.send_message(message.channel, resposta.replace("a", ":regional_indicator_a:"))#.replace("b", ":regional_indicator_b:").replace("c", ":regional_indicator_c:").replace("d", ":regional_indicator_d:").replace("e", ":regional_indicator_e:").replace("f", ":regional_indicator_f:").replace("g", ":regional_indicator_g:").replace("h", ":regional_indicator_h:").replace("i", ":regional_indicator_i:").replace("j", ":regional_indicator_j:").replace("k", ":regional_indicator_k:").replace("l", ":regional_indicator_l:").replace("m", ":regional_indicator_m:").replace("n", ":regional_indicator_n:").replace("o", ":regional_indicator_o:").replace("p", ":regional_indicator_p:").replace("q", ":regional_indicator_q:").replace("r", ":regional_indicator_r:").replace("s", ":regional_indicator_s:").replace("t", ":regional_indicator_t:").replace("u", ":regional_indicator_u:").replace("v", ":regional_indicator_v:").replace("w", ":regional_indicator_w:").replace("x", ":regional_indicator_x:").replace("y", ":regional_indicator_y:").replace("z", ":regional_indicator_z:"))   
+            await client.send_message(message.channel, "{}".format(resposta.replace("a", ":regional_indicator_a:").replace("b", ":regional_indicator_b:").replace("c", ":regional_indicator_c:").replace("d", ":regional_indicator_d:").replace("e", ":regional_indicator_e:").replace("f", ":regional_indicator_f:").replace("g", ":regional_indicator_g:").replace("h", ":regional_indicator_h:").replace("i", ":regional_indicator_i:").replace("j", ":regional_indicator_j:").replace("k", ":regional_indicator_k:").replace("l", ":regional_indicator_l:").replace("m", ":regional_indicator_m:").replace("n", ":regional_indicator_n:").replace("o", ":regional_indicator_o:").replace("p", ":regional_indicator_p:").replace("q", ":regional_indicator_q:").replace("r", ":regional_indicator_r:").replace("s", ":regional_indicator_s:").replace("t", ":regional_indicator_t:").replace("u", ":regional_indicator_u:").replace("v", ":regional_indicator_v:").replace("w", ":regional_indicator_w:").replace("x", ":regional_indicator_x:").replace("y", ":regional_indicator_y:").replace("z", ":regional_indicator_z:").replace("1", "⃣").replace("2", "⃣").replace("3", "3⃣").replace("4", "4⃣").replace("5", "5⃣").replace("6", "6⃣").replace("7", "7⃣").replace("8", "8⃣").replace("9", "9⃣").replace("0", "0⃣").replace("#", "#⃣")))   
         
 
         if message.content.startswith("/discorddott"):
@@ -253,6 +252,23 @@ async def on_message(message):
                     embed.set_image(url="https://pbs.twimg.com/media/Dh8UfXjW0AEps6n.jpg")
                     embed.timestamp = datetime.datetime.utcnow()
                     await client.send_message(canal, embed=embed)
+
+        if message.content.startswith("/voltavaga"):
+            cargos = [
+                # IDs dos cargos:
+                "407677666750365706", #Diretor
+            ]
+            for r in message.author.roles:
+                if r.id in cargos:
+                    await asyncio.sleep(1)
+                    await client.delete_message(message)
+                    await asyncio.sleep(1)
+                    canal = client.get_channel("448326186095869953")
+                    await client.send_message(canal, "@everyone")
+                    embed= discord.Embed(
+                        color=COR,
+                        description="**Temporada de aplicação à equipe**\n\nA temporada de aplicação à equipe está aberta novamente! E aí, quer m"
+                    )
 
         if message.content.lower().startswith("/testtest"):
             try:
@@ -1742,7 +1758,7 @@ async def on_message(message):
                             embed = discord.Embed(
                             title='Informações:',
                             color=COR,
-                            description="**Usuário**: `{}`\n**ID**: `{}`\n**Motivo**: `{}`\n**Punição**: Tempmute\n**Duração**: `{}s`\n\n**Autor**: {}\n**Canal**: {}".format(membro.name, membro.id, motivo, segundos, message.author.mention, message.channel.mention)
+                            description="**Usuário**: `{}`\n**ID**: `{}`\n**Motivo**: `{}`\n**Punição**: Tempmute\n**Duração**: `{}`\n\n**Autor**: {}\n**Canal**: {}".format(membro.name, membro.id, motivo, segundos, message.author.mention, message.channel.mention)
                             )
                             embed.set_author(name="PUNIÇÃO", icon_url="https://i.imgur.com/1iJeEea.jpg")
                             embed.set_thumbnail(url=message.author.avatar_url)
@@ -1763,7 +1779,7 @@ async def on_message(message):
                             embed = discord.Embed(
                             title='Informações:',
                             color=COR,
-                            description="**Usuário**: `{}`\n**ID**: `{}`\n**Motivo**: `{}`\n**Punição**: Tempmute\n**Duração**: `{}m`\n\n**Autor**: {}\n**Canal**: {}".format(membro.name, membro.id, motivo, minutos, message.author.mention, message.channel.mention)
+                            description="**Usuário**: `{}`\n**ID**: `{}`\n**Motivo**: `{}`\n**Punição**: Tempmute\n**Duração**: `{}`\n\n**Autor**: {}\n**Canal**: {}".format(membro.name, membro.id, motivo, minutos, message.author.mention, message.channel.mention)
                             )
                             embed.set_author(name="PUNIÇÃO", icon_url="https://i.imgur.com/1iJeEea.jpg")
                             embed.set_thumbnail(url=message.author.avatar_url)
@@ -1783,7 +1799,7 @@ async def on_message(message):
                             embed = discord.Embed(
                             title='Informações:',
                             color=COR,
-                            description="**Usuário**: `{}`\n**ID**: `{}`\n**Motivo**: `{}`\n**Punição**: Tempmute\n**Duração**: `{}h`\n\n**Autor**: {}\n**Canal**: {}".format(membro.name, membro.id, motivo, horas, message.author.mention, message.channel.mention)
+                            description="**Usuário**: `{}`\n**ID**: `{}`\n**Motivo**: `{}`\n**Punição**: Tempmute\n**Duração**: `{}`\n\n**Autor**: {}\n**Canal**: {}".format(membro.name, membro.id, motivo, horas, message.author.mention, message.channel.mention)
                             )
                             embed.set_author(name="PUNIÇÃO", icon_url="https://i.imgur.com/1iJeEea.jpg")
                             embed.set_thumbnail(url=message.author.avatar_url)
