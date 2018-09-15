cd331f7071c71e1edaddbf1cc9043393c53351f8
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-15 15:41:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1506,7 +1506,7 @@ async def on_message(message):
         if message.content.lower().startswith("/emojizar"):
             args = message.content.split(" ")
             text = " ".join(args[1:])
-            emojified = '⬇ Copie e cole aqui: ⬇\n'
+            emojified = ' '
             formatted = re.sub(r'[^A-Za-z ]+', "", text).lower()
             if text == '':
                 await client.send_message(message.channel, 'Lembre-se de dizer o que você deseja converter!')
