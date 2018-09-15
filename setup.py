8eb246a3cd9a9689809ee907b0d115cd9a989e87
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-15 15:54:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1516,8 +1516,8 @@ async def on_message(message):
                         emojified += '     '
                     else:
                         emojified += ':regional_indicator_{}: '.format(i)
-                if len(emojified) + 2 >= 2000:
-                    await client.send_message(message.channel, 'Sua mensagem em emoticons excede 2000 caracteres!')
+                if len(emojified) + 2 >= 200:
+                    await client.send_message(message.channel, 'Sua mensagem em emoticons excede 200 caracteres!')
                 if len(emojified) <= 25:
                     await client.send_message(message.channel, 'Sua mensagem não pôde ser convertida!')
                 else:
