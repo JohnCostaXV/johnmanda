836b46f014a7ecd37a5343ddc1ab818cb933d712
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-09-20 11:32:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1920,7 +1920,7 @@ async def on_reaction_add(reaction, user):
     msg = reaction.message
 
     if reaction.emoji == "✅" and msg.id == msg_id: #and user == msg_user:
-     await client.remove_reaction(msg, "✅", user)
+     #await client.remove_reaction(msg, "✅", user)
      role1 = discord.utils.get(user.server.roles, name="Unregister")
      await client.remove_roles(user, role1)
      
